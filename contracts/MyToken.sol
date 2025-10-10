ae480716ed153195d0f0147c71fb075dc6e7c1b4
dxssie <dxssix@naver.com>
2025-10-10 22:34:18 +0900

    mint function

---
+++ contracts/MyToken.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifer: MIT
+pragma solidity ^0.8.28;
+
+contract MyToken {
+    string public name;
+    string public symbol;
+    uint8 public decimals; // uint8 --> 8 bit unsigned int, uint16, ... , unint 256
+
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+
+    constructor(string memory _name, string memory _symbol, uint8 _decimal) {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimal;
+        _mint(1*10**uint256(decimals), msg.sender); // 1 MT
+    }
+
+    function _mint(uint256 amount, address owner) internal {
+        totalSupply += amount;
+        balanceOf[owner] += amount;
+    } 
+
+}
